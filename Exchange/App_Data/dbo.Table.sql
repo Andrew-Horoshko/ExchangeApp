b2b509e0f21859af899879eee30b4d786776dc14
Andrew-Horoshko <69959083+Andrew-Horoshko@users.noreply.github.com>
2021-01-21 10:37:36 +0200

    Delete dbo.Table.sql

--- Exchange/App_Data/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ExhangeHistory]
-(
-	[Id] INT NOT NULL  IDENTITY  PRIMARY KEY ,
-	[FromCurrency] VARCHAR(3) NOT NULL ,
-	[FromAmount] DECIMAL(15,2) NOT NULL,
-	[ToCurrency] VARCHAR(3) NOT NULL ,
-	[ToAmount] DECIMAL(15,2) NOT NULL,
-	[Date] DATE NOT NULL
-)
